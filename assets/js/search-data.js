ab30e1bbd4e84a92d9d1eac8f9c47d03a2de0c97
profsergiocosta <sergio.costa@ufma.br>
2026-03-06 12:45:12 +0000

    Deploying to gh-pages from @ LambdaGeo/lambdageo.github.io@5fcb76a3048aabee538b08a410a403908f877f19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/software/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Core Courses (English Summaries).",
