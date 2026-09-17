8262ffb356a456c3ee4ed50c904921460f6c259a
profsergiocosta <sergio.costa@ufma.br>
2026-09-17 11:14:51 +0000

    Deploying to gh-pages from @ LambdaGeo/lambdageo.github.io@d20fca2a13215e5ff69197bae50b8a29de5c29bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,13 +58,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "https://lambdageo.github.io/geospatial-modeling-python/";
               },
-            },{id: "dropdown-dissmodel-book",
-              title: "DisSModel Book",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "https://dissmodel.github.io/dissmodel-book/";
-              },
             },{id: "dropdown-tutoriais",
               title: "Tutoriais",
               description: "",
